845fe634813b707ab5e7e55957a7cca802b64f85
myus3rnam3159 <le3akmailg@gmail.com>
2024-06-07 12:47:52 +0700

    test I01 ok

--- ruleValidation/IMO/IMO.ts
+++ ruleValidation/IMO/IMO.ts
@@ -70,7 +70,7 @@ export function getI02Validation(selectedDate: SelectedDate, workRecords: WorkRe
             const subEndI: number = i + 48 - 1;
             const subS: string = mergeTB.value.slice(i, subEndI + 1);
 
-            if(subS.match(/^(?:[^1]*1){29}/g) !== null){
+            if(subS.match(/(?:[^1]*1){29}/g) !== null){
                 violatedRange.start = i;
                 violatedRange.end = subEndI;
                 break;
@@ -110,7 +110,47 @@ export function getI06Validation(selectedDate: SelectedDate, workRecords: WorkRe
             const subEndI: number = i + 48 - 1;
             const subS: string = mergeTB.value.slice(i, subEndI + 1);
 
-            if(subS.match(/^(?:[^0]*0{1,}[^0]*0{12,}[^0]*|[^0]*0{12,}[^0]*0{1,}[^0]*)$/g) === null){
+            if(subS.match(^(?:[^0]*0{1,}[^0]*0{12,}[^0]*|[^0]*0{12,}[^0]*0{1,}[^0]*)$/g) === null){
+                violatedRange.start = i;
+                violatedRange.end = subEndI;
+                break;
+            }
+            i = subEndI - 1;
+        }
+    }
+    const nCIndices: number[] = getNCIndice(violatedRange, selectedRange);
+    return editNCIndice(nCIndices, mergeTB.value);
+}
+
+export function getI01Validation(selectedDate: SelectedDate, workRecords: WorkRecord[]): string | null {
+
+    const checkDates: SurroundingDates = getSurroundingDates(selectedDate.selectedDate, workRecords.map(wr => wr.workDate),1);
+
+    const leftMergedTimebar: string = getSurroundTimebar(
+        checkDates.prevDates, 
+        workRecords.map(wr => wr.workDate), 
+        workRecords.map(wr => wr.workRecordValue)
+        );
+
+    const rightMergedTimebar: string = getSurroundTimebar(
+        checkDates.nextDates, 
+        workRecords.map(wr => wr.workDate), 
+        workRecords.map(wr => wr.workRecordValue)
+        );
+
+    const mergeTB: MergedTimeBar = mergeTimeBars(selectedDate, leftMergedTimebar, rightMergedTimebar);
+    const affectedIndices: AffectableIndices = getAffectableIndices(mergeTB, 48);
+
+    const selectedRange: number[] = getSelectedRange(mergeTB.startSelIdx, mergeTB.endSelIdx);
+    let violatedRange: ViolatedRange = {start: -1, end: -1};
+
+    for(let i = affectedIndices.affectedStartIdx; i <= affectedIndices.affectedEndIdx; i++){
+        if(mergeTB.value[i] === '1'){
+
+            const subEndI: number = i + 48 - 1;
+            const subS: string = mergeTB.value.slice(i, subEndI + 1);
+
+            if(subS.match(/0{12,}/g) === null){
                 violatedRange.start = i;
                 violatedRange.end = subEndI;
                 break;
